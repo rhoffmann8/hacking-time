f7c7c7714b09c369029e6694f94639837f50b411
Rob Hoffmann <rhoffmann8@gmail.com>
2015-05-29 00:23:43 -0400

    It's hacking time

---
+++ hacktime.java
@@ -0,0 +1,52 @@
+/*
+ * How to Hack Time, as seen in Kung Fury
+ * https://www.youtube.com/watch?v=bS5P_LAqiVg
+ */
+
+/*
+ * First time the code appears (10:29). Code is also present
+ * in the "E = mc2 CONVERTER" to the right, but it's just snippets
+ * of the same code from the main terminal.
+ */
+DateTime warp = new DateTime(12, 33.9, 15);
+warp.accesDimension(4);
+for (int i = 0 ; i < warp.getGoal() ; i++ ){
+    double e = Math.power( m*c , 2);
+    warp.hackTime()
+    warp.advance(i.toTimeInterval())
+}
+if ( past.currentDate() == warp.currentDate() ) {
+    System.out.println("Time Hacked successfully.");
+} else {
+    // Before the camera cuts away, I could make out
+    // "System.out.println("Re"" and assumed it was leading
+    // up to System.out.println("Retry time-hacking, new variables");
+    System.out.println("Retry time-hacking, new variables");
+
+/*
+ * Second time we get a good look (10:47). Since this snippet begins
+ * with "System.out.println("Retry time-hacking, new variables");" I figured
+ * it was a continuation of the previous one, but the end brace after the 
+ * E = mc^3 followed by another else would be syntactically incorrect.
+ * Also, so would "sdfsdfsdfjadnfkjafna..."
+ */
+System.out.println("Retry time-hacking, new variables");
+double E = Math.power ( m*c, 3);
+}
+System.out.println("Time Hacked successfully.");
+} else {
+warp.accesDimension(4);
+for (int i = 0 ; i < warp.getGoal() ; i++) {
+    double e = Math.power( m*c , 2);
+    warp.hackTime();
+    warp.advance(1.toTimeInterval());
+// The first column ends here, following code is on the second column
+warp.accesDimension(4);
+for (int i = 0 ; i < warp.getGoal() ; i++) {
+    double e = Math.power( m*c , 2);
+    warp.hackTime();
+    warp.advance(1.toTimeInterval());
+    sdfsdfsdfjadnfkjafnajklnasdkljfnasdfjkasndfjklasnfjklasnfkljasdnklasdjnagdngfjklasdnadklgnjasdk
+
+// We catch another glimpse at the E = mc2 CONVERTER at (10:49), but again
+// it's just repeated code from before.
